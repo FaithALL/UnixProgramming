27cf19cbcbef596051ce943ebe9a4b6f2630e5e7
guoqinag <1915763994@qq.com>
2020-10-28 15:29:16 +0800

    修改echo实现

--- socket/echo/echoclient.c
+++ socket/echo/echoclient.c
@@ -1,42 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <assert.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 3) {
         fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
         return 0;
     }
+    const char *ip = argv[1];
+    const int port = atoi(argv[2]);
 
-    const char* ip = argv[1];
-    const int  port = atoi(argv[2]);
-    
     //创建ipv4 socket
-    int clientfd = socket(PF_INET, SOCK_STREAM, 0);
+    int clientfd = socket(AF_INET, SOCK_STREAM, 0);
     assert(clientfd >= 0);
-
     //创建server sock地址
     struct sockaddr_in server;
     server.sin_family = AF_INET;
     inet_pton(AF_INET, ip, &server.sin_addr);
     server.sin_port = htons(port);
 
-    if (connect(clientfd, (struct sockaddr*)&server, sizeof(server)) != 0) {
+    if (connect(clientfd, (struct sockaddr *) &server, sizeof(server)) != 0) {
         fprintf(stderr, "failed to connect server\n");
         return 0;
     }
 
     char buf[1024];
-    while (fgets(buf, 1024, stdin) != NULL) {
-        send(clientfd, buf, strlen(buf), 0);
-        recv(clientfd, buf, strlen(buf), 1024);
-        fputs(buf, stdout);
+    int count;
+    //从终端读取
+    while ((count = read(STDIN_FILENO, buf, 1024)) != 0) {
+        write(clientfd, buf, count);
+        recv(clientfd, buf, count, MSG_WAITALL);
+        write(STDOUT_FILENO, buf, count);
     }
 
     close(clientfd);
--- socket/echo/echoserver.c
+++ socket/echo/echoserver.c
@@ -1,56 +1,50 @@
+/**
+ * 创建echo服务器
+ */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <assert.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "usage: %s <port>\n", argv[0]);
         return 0;
     }
-
     const int port = atoi(argv[1]);
-    const char* ip = "127.0.0.1";
-    
+
     int serverfd = socket(AF_INET, SOCK_STREAM, 0);
     assert(serverfd >= 0);
 
     struct sockaddr_in address;
     address.sin_family = AF_INET;
-    inet_pton(AF_INET, ip, &address.sin_addr);
+    address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
 
-    int ret = bind(serverfd, (struct sockaddr*)&address, sizeof(address));
+    //绑定socket地址
+    int ret = bind(serverfd, (struct sockaddr *) &address, sizeof(address));
     assert(ret != -1);
-    
+
     ret = listen(serverfd, 5);
     assert(ret != -1);
 
     while (1) {
         struct sockaddr_in client;
         socklen_t clientlength = sizeof(client);
-        int clientsock = accept(serverfd, (struct sockaddr*)&client, &clientlength);
+        int clientsock = accept(serverfd, (struct sockaddr *) &client, &clientlength);
         assert(clientsock >= 0);
 
-        char remote[INET_ADDRSTRLEN];
-        printf("connected with ip: %s and port: %d\n", 
-                inet_ntop(AF_INET, &client.sin_addr, remote, INET_ADDRSTRLEN), ntohs(client.sin_port));
-
         char buf[1024];
-        while (recv(clientsock, buf, 1024, 0) != 0) {
-            printf("-->%s", buf);
-            if (strlen(buf) >= 3) {
-                buf[0] = '<';
-                buf[2] = buf[1] = '-';
-            }
-            send(clientsock, buf, strlen(buf), 0);
+        int count;
+        while ((count = read(clientsock, buf, 1024)) != 0) {
+            write(STDOUT_FILENO, buf, count);
+            write(clientsock, buf, count);
         }
-        close(serverfd);
+        close(clientsock);
     }
+
+    close(serverfd);
     return 0;
 }
