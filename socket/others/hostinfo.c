fc7a4f480dd679385015c36b88702c740beac014
guoqinag <1915763994@qq.com>
2020-12-10 00:08:27 +0800

    增加注释信息

--- socket/others/hostinfo.c
+++ socket/others/hostinfo.c
@@ -30,6 +30,7 @@ int main(int argc, char *argv[]) {
     //显示地址而非域名
     flags = NI_NUMERICHOST;
     for (p = listp; p; p = p->ai_next) {
+        //转换为点分十进制
         getnameinfo(p->ai_addr, p->ai_addrlen, buf, MAXLINE, NULL, 0, flags);
         printf("%s\n", buf);
     }
