9e33173a5a4ec9f5f0d645042a5679f09a64dd9f
guoqinag <1915763994@qq.com>
2020-12-11 00:03:24 +0800

    CGI程序：求两个数的和

---
+++ socket/csapp/cgi_adder.c
@@ -0,0 +1,36 @@
+//CGI程序，对两个参数求和，返回带结果的HTML文件给客户端
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define MAXLINE 1024
+
+int main() {
+    char *buf, *p;
+    char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+    int n1 = 0, n2 = 0;
+
+    if ((buf = getenv("QUERY_STRING")) != NULL) {
+        p = strchr(buf, '&');
+        *p = '\0';
+        strcpy(arg1, buf);
+        strcpy(arg2, p + 1);
+        n1 = atoi(arg1);
+        n2 = atoi(arg2);
+    }
+
+    sprintf(content, "QUERY_STRING=%s", buf);
+    sprintf(content, "Welcome to add.com: ");
+    sprintf(content, "%sThe Internet addition portal.\r\n<p>", content);
+    sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
+    sprintf(content, "%sThanks for visiting!\r\n", content);
+
+    printf("Connection: close\r\n");
+    printf("Content-length: %d\r\n", (int)strlen(content));
+    printf("Content-type: text/html\r\n\r\n");
+    printf("%s", content);
+    fflush(stdout);
+    return 0;
+}
+
